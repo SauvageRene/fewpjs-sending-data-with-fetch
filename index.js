3827b4031736064332592d0ca6983494a6e96706
Rene <renegarcia1864@gmail.com>
2021-06-28 15:19:50 -0500

    commit tree 59ce38cccd7fe89ed230d40a197330bc42d312ae

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
+
+// function submitData(name, email) {
+//     return fetch("http://localhost:3000/users", configObject)
+//     .then(function(response) {
+//         return response.json();
+//       })
+//       .then(function(object) {
+//         document.body.innerHTML = object["id"];
+//       })
+//       .catch(function(error) {
+//         document.body.innerHTML = error.message;
+//       }); 
+
+// };
+
+// const configObject = {
+//     method:"POST",
+//     headers: {
+//         "Content-type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// };
+
+// const formData = {
+//     name:,
+//     email: 
+// };
